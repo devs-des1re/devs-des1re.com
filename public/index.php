aec59a72f8f734a737958d69f3111b062efced91
devs_des1re <arjunbrij8811@gmail.com>
2025-12-08 18:19:58 +0000

    Added APIs to the website

--- public/index.php
+++ public/index.php
@@ -52,6 +52,16 @@
                 </p>
                 <a href="https://devs-des1re.com/donate" class="flex justify-center items-center mt-2 text-[#0099FF] transition-colors duration-300 hover:text-[#0033FF]">Visit</a>
             </div>
+
+            <div class="w-full sm:w-80 px-4 py-3 bg-[#FFFFFF] rounded-md shadow-md">
+                <h1 class="mt-2 text-lg font-semibold text-center">APIs</h1>
+                <p class="mt-2 text-sm text-[#3C3C3C]">
+                    If you want free open source APIs, with
+                    no cost and just want a reliable service,
+                    I provide it for free. Here I host multiple APIs!
+                </p>
+                <a href="https://api.devs-des1re.com/" class="flex justify-center items-center mt-2 text-[#0099FF] transition-colors duration-300 hover:text-[#0033FF]">Visit</a>
+            </div>
         </div>
     </main>
 
